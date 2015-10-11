7e4302c942fa45ef24277813a41d71ecd0f19dc2
“Alizaybak” <dacker@brandeis.edu>
2015-10-11 18:24:42 -0400

    _

--- Plasmid/MainVC.swift
+++ Plasmid/MainVC.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class MainVC: UIViewController, UITextViewDelegate
+class MainVC: UIViewController, UITextViewDelegate, UISearchBarDelegate
 {
 
   @IBOutlet weak var textView: UITextView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var dismissBtnOut: UIButton!
 
   let toolsActionView = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
   
   override func viewDidLoad()
   {
     super.viewDidLoad()
-
+    
+    self.dismissBtnOut.hidden = true
     self.textView.delegate = self
     let annotateAction = UIAlertAction(title: "Annotate", style: UIAlertActionStyle.Default)
       {
@@ -35,6 +38,11 @@ class MainVC: UIViewController, UITextViewDelegate
         action in
         self.performSegueWithIdentifier("inspectSegue", sender: nil)
     }
+    let scanAction = UIAlertAction(title: "Scan", style: UIAlertActionStyle.Default)
+      {
+        action in
+        self.performSegueWithIdentifier("scanSegue", sender: nil)
+    }
     let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel)
       {
         action in
@@ -42,6 +50,7 @@ class MainVC: UIViewController, UITextViewDelegate
     self.toolsActionView.addAction(annotateAction)
     self.toolsActionView.addAction(translateAction)
     self.toolsActionView.addAction(inspectAction)
+    self.toolsActionView.addAction(scanAction)
     self.toolsActionView.addAction(cancelAction)
     NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateTextView", name: "newAttributedString", object: nil)
   }
@@ -56,6 +65,35 @@ class MainVC: UIViewController, UITextViewDelegate
     self.textView.attributedText = Global.seqAttributedString
   }
   
+  // MARK : Search bar delegate
+  // Untested ...
+  func searchBarSearchButtonClicked(searchBar: UISearchBar)
+  {
+    let mutable = NSMutableAttributedString(attributedString: Global.seqAttributedString)
+    let searchRegex = NSRegularExpression(pattern: searchBar.text, options: .CaseInsensitive, error: nil)
+    let sequenceString = Global.activeSeqObject.sequence
+    let matches = searchRegex?.matchesInString(sequenceString, options: nil, range: NSMakeRange(0, count(sequenceString)))
+    if matches?.count > 0
+    {
+      for match in matches!
+      {
+        let range = match.range
+        mutable.addAttribute(NSBackgroundColorAttributeName, value: UIColor.yellowColor(), range: range)
+      }
+      self.textView.attributedText = mutable
+      self.textView.scrollRangeToVisible(matches![0].range)
+      // Show a way to dismiss highlighting
+      self.dismissBtnOut.hidden = false
+      self.searchBar.resignFirstResponder()
+    }
+  }
+  
+  @IBAction func dismissBtn(sender: AnyObject)
+  {
+    self.textView.attributedText = Global.seqAttributedString
+    self.dismissBtnOut.hidden = true
+  }
+  
   // MARK : Text view delegate
   func textViewDidChangeSelection(textView: UITextView)
   {
@@ -67,4 +105,5 @@ class MainVC: UIViewController, UITextViewDelegate
   {
     self.presentViewController(self.toolsActionView, animated: true, completion: nil)
   }
+  
 }
---
+++ Plasmid/ScanVC.swift
@@ -0,0 +1,21 @@
+//
+//  ScanVC.swift
+//  Plasmid
+//
+//  Created by Daniel Acker on 10/11/15.
+//  Copyright (c) 2015 Daniel Acker. All rights reserved.
+//
+
+import UIKit
+
+class ScanVC: UIViewController
+{
+  
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    
+    self.navigationController?.navigationBarHidden = false
+  }
+  
+}
